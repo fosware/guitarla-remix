be85b51fa26472789ff243e46651fc21d3aee01b
Mario González <mariogonzalez@iMac-de-Mario.local>
2022-11-18 18:56:43 -0600

    created own root file

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,35 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+    Meta
+} from '@remix-run/react'
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export function meta(){
+    return (
+        {
+            charset: 'utf-8',
+            title: 'GuitarLA Remix',
+            viewport: 'width=device-width,initial-scale=1'
+        }
+    )
+}
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <Document>
+            <h1>Dese App</h1>
+        </Document>
+
+    )
+}
+
+function Document ({children}) {
+    return(
+        <html lang="es">
+            <head>
+               <Meta />   
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
 }
--- app/routes/index.jsx
+++
@@ -1,5 +0,0 @@
-export default function Index() {
-  return (
-    <h1>Hola mundo desde Remix!</h1>
-  );
-}
